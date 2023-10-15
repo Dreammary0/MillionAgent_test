7cd9c76deb6fceceb84588133dc0bc4a2d65be2d
Dreammary0 <sazontova.md@students.dvfu.ru>
2023-10-15 16:23:32 +1000

    выбор городов (потоки)

---
+++ Product.py
@@ -0,0 +1,74 @@
+import pandas as pd
+from hashlib import md5
+from requests import Session
+
+class Product:
+    def __init__(self, id, name, brand, link, regular_price, promo_price, sale=None, city = None):
+        self.id = id
+        self.name = name
+        self.brand = brand
+        self.link = link
+        self.regular_price = regular_price
+        self.promo_price = promo_price
+        self.sale = sale
+        self.city = city
+
+    @staticmethod
+    def from_json(data, city):
+        if data["price"]["basePrice"] != data["price"]["actual"]:
+            sale = 'True'
+        else:
+            sale = 'False'
+        return Product(
+            id=data["id"],
+            name=data["title"],
+            brand=data["brand_name"],
+            link=data["webpage"],
+            regular_price=data["price"]["basePrice"],
+            promo_price=data["price"]["actual"],
+            sale=sale,
+            city = city
+        )
+
+    @staticmethod
+    def to_df(parse_list):
+        data = {
+            'id': [p.id for p in parse_list],
+            'name': [p.name for p in parse_list],
+            'brand': [p.brand for p in parse_list],
+            'link': [p.link for p in parse_list],
+            'regular_price': [p.regular_price for p in parse_list],
+            'promo_price': [p.promo_price for p in parse_list],
+            'sale': [p.sale for p in parse_list],
+            'city': [p.city for p in parse_list]
+
+        }
+        return pd.DataFrame(data)
+
+
+def generation_fingerprint(city):
+    sess = Session()
+    cookies = {
+        'selected_city_code': city[0],
+    }
+    headers = {
+        'Host': '4lapy.ru',
+        'User-Agent': 'lapy 3.0.0 (iPhone; IOS 17.0.3; Scale/3.00)',
+        'Version-Build': '3.0.0',
+        'Authorization': 'Basic NGxhcHltb2JpbGU6eEo5dzFRMyhy',
+        'X-Apps-Build': '3.0.0',
+        'X-Apps-Os': '17.0.3',
+        'X-Apps-Screen': '2778x1284',
+        'X-Apps-Device': 'iPhone14,3',
+        'X-Apps-Location': f'lat:{city[1]},lon:{city[2]}',
+        'X-Apps-Additionally': '404',
+        'Connection': 'close',
+    }
+    token = sess.get('https://4lapy.ru/api/start/', headers=headers, cookies=cookies)
+    return sess, token.json()['data']['token'], headers, cookies
+
+def get_sign(data):
+    elements = [md5(str(i).encode()).hexdigest() for i in data.values()]
+    elements.sort()
+    return md5(('ABCDEF00G' + ''.join(elements)).encode()).hexdigest()
+
--- main.py
+++ main.py
@@ -1,90 +1,49 @@
+import requests
 import pandas as pd
-from Parse import Parse
-# import multiprocessing
-import time
-import asyncio
-import aiohttp
+import json
+from Product import Product, generation_fingerprint, get_sign
+import concurrent.futures
 
-start_time = time.time()
-async def process_url(session, url, page_number):
-    print(page_number)
-    productList = []
-    try:
-        async with session.get(url.format(page_number)) as resp:
-            resp.raise_for_status()
-            data = await resp.json()
-            goods_data = data.get("data", {}).get("goods", [])
-            for item in goods_data:
+def fetch_city(city):
+    sess, token, headers, cookies = None, None, None, None
+    products = []
+    page_namber = 1
+    while True:
+        if not sess:
+            sess, token, headers, cookies = generation_fingerprint(cities[city])
+        params = {
+            'sort': 'popular',
+            'category_id': 32,
+            'page': page_namber,
+            'count': 10,
+            'token': token,
+        }
+        params['sign'] = get_sign(params)
+        try:
+            response = sess.get('https://4lapy.ru/api/goods_list_cached/', params=params, cookies=cookies,
+                                headers=headers).json()
+            print(page_namber, end=" " )
+            # Создаем список словарей с необходимыми данными
+            for item in response["data"]["goods"]:
                 if item["availability"] == 'В наличии':
-                    productList.append(Parse.from_json(item))
-            return productList
-    except aiohttp.ClientError as e:
-        print("Ошибка:", e)
-        return []
-
-async def main():
-    async with aiohttp.ClientSession() as session:
-        url = "https://4lapy.ru/api/goods_list_cached/?&category_id=32&count=10&page={}&sign=e16816f878c65a77949913e9510d4568&sort=popular&token=7998cc7b5a58d8583214cc17a4a247a7"
-        async with session.get(url.format(1)) as resp:
-            data = await resp.json()
-            max_pages = data['data']['total_pages']
-
-        tasks = []
-        for page_number in range(1,max_pages+1):
-            task = asyncio.create_task(process_url(session, url, page_number))
-            tasks.append(task)
-
-        results = await asyncio.gather(*tasks)
-        productList = []
-        for result in results:
-            productList.extend(result)
-        df = Parse.to_df(productList)
-        df.to_excel("products.xlsx", index=False)
-
-asyncio.run(main())
-print("--- %s seconds ---" % (time.time() - start_time))
+                    products.append(Product.from_json(item, city))
+            page_namber += 1
+            if page_namber > int(response['data']['total_pages']) :
+                break
+        except Exception as e:
+            print("Ошибка:", e)
+            continue
+    return Product.to_df(products)
 
 
+cities = {
+    'Москва': ('0000073738', 55.755819, 37.617644),
+    'Санкт-Петербург': ('0000103664', 59.939095, 30.315868)
+}
 
-
-# def process_url(url, cookies):
-#     productList = []
-#     page_number = 1
-#     while True:
-#         try:
-#             response = requests.get(url.format(page_number))
-#             response.raise_for_status()
-#             data = response.json()
-#             goods_data = data.get("data", {}).get("goods", [])
-#             for item in goods_data:
-#                 if item["availability"] == 'В наличии':
-#                     productList.append(Parse.from_json(item))
-#             if page_number == data["data"]["total_pages"]:
-#                 break
-#             page_number += 1
-#         except requests.exceptions.RequestException as e:
-#             print("Ошибка:", e)
-#             continue
-#     return Parse.to_df(productList)
-#
-# def process_url_wrapper(args):
-#     return process_url(*args)
-#
-# if __name__ == '__main__':
-#     url1 = "https://4lapy.ru/api/goods_list_cached/?&category_id=32&count=10&page={}&sign=e16816f878c65a77949913e9510d4568&sort=popular&token=7998cc7b5a58d8583214cc17a4a247a7"
-#     url2 = "https://4lapy.ru/api/goods_list_cached/?category_id=32&count=10&page={}&sign=a793d334aa5bf059b73f1c18c2db20f7&sort=popular&token=7998cc7b5a58d8583214cc17a4a247a7"
-#     cookies1 = {'selected_city_code': '0000073738'}
-#     cookies2 = {'selected_city_code': '0000103664'}
-#
-#     with multiprocessing.Pool(processes=2) as pool:
-#         results = pool.map(process_url_wrapper, [(url1, cookies1), (url2, cookies2)])
-#
-#     df1 = results[0]
-#     df2 = results[1]
-#     df3 = pd.merge(df1, df2, on=['id', 'name', 'brand', 'link', 'regular_price', 'promo_price', 'sale'])
-#
-#     df3.to_excel("products.xlsx", index=False)
-#     print(len(df1), len(df2), len(df3))
-#     print(df3)
-
-
+def start():
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = list(executor.map(fetch_city, cities))
+    result = pd.concat(results)
+    result.to_excel("products.xlsx", index=False)
+start()
